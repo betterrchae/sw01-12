b40bc64ae9c272238cc8c10ce0ef8b4e7b8ccafa
ii2001 <iicks2001@gmail.com>
2025-05-17 07:27:59 +0900

    :recycle: switch문에 맞게 변경

--- app/Model/Strategy/RandomYutThrowStrategy.java
+++ app/Model/Strategy/RandomYutThrowStrategy.java
@@ -9,7 +9,6 @@ public class RandomYutThrowStrategy implements YutThrowStrategy {
 
     @Override
     public YutResult throwYut() {
-        // 윷 4개를 던져서 앞/뒤 계산
         int frontCount = 0;
         for (int i = 0; i < 4; i++) {
             if (random.nextBoolean()) {
@@ -18,20 +17,12 @@ public class RandomYutThrowStrategy implements YutThrowStrategy {
         }
 
         // 앞면 개수에 따른 결과 결정
-        switch (frontCount) {
-            case 0:
-                return YutResult.MO;
-            case 1:
-                return YutResult.DO;
-            case 2:
-                return YutResult.GAE;
-            case 3:
-                return YutResult.GEOL;
-            case 4:
-                return YutResult.YUT;
-            default:
-                // 빽도 (추가 랜덤 확률로 등장)
-                return YutResult.DO;
-        }
+        return switch (frontCount) {
+            case 0 -> YutResult.MO;
+            case 2 -> YutResult.GAE;
+            case 3 -> YutResult.GEOL;
+            case 4 -> YutResult.YUT;
+            default -> YutResult.DO;
+        };
     }
 }
